ee93f9562300923f0007d02c96b1bd8ed57272ea
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-01-06 14:17:06 +0100

    fix system solution

--- testScript.m
+++ testScript.m
@@ -1,14 +1,14 @@
-f1 = @(x, y1, y2)(3 * exp(x / 2) - y2);
-f2 = @(x, y1, y2)(y2);
+f1 = @(x, y1, y2)(y2);
+f2 = @(x, y1, y2)(3 * exp(x / 2) - y2);
 a = 0;
-b = 5;
-n = 10;
+b = 10;
+n = 100;
 
 y1a = 4;
 y2a = 3;
 
 y1Solution = @(x)(-exp(-x) + 4 * exp(x / 2) + 1);
-y2Solution = @(x)(exp(x) + 2 * exp(x / 2));
+y2Solution = @(x)(exp(-x) + 2 * exp(x / 2));
 
 [x, y1, y2] = solveDifferentialSystem(f1, f2, a, b, n, y1a, y2a);
 
